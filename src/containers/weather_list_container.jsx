d80d2b525cb142ebd8d63ef03df27dcc3fcf43c6
hitro11 <rohitnazareth07@gmail.com>
2018-06-15 20:49:26 -0400

    added graphs for each measurement + improved style

--- src/containers/weather_list_container.jsx
+++ src/containers/weather_list_container.jsx
@@ -1,41 +1,56 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import { bindActionCreators } from 'redux';
 
-import {fetchWeather} from '../actions/index';
+import Graph from '../components/graph'
+
 
 class WeatherList extends Component {
 
   constructor(props) {
      super(props);
-     this.state = {term: ''};
+
+     this.renderWeather.bind(this);
   }
 
   render() {
     return (
-        <table className="table table-hover">
-            <thead>
+        <table className="table table-borderless table-hover ">
+            <thead className="">
                 <tr>
                     <th>City</th>
-                    <th>Temperature</th>
-                    <th>Pressure</th>
-                    <th>Humidity</th>
+                    <th>Temperature (&deg; C)</th>
+                    <th>Humidity (%)</th>
+                    <th>Wind Speed (km/h)</th>
                 </tr>
-            </thead>
-            
+            </thead>            
             <tbody>
-
+               {this.props.weather.map(this.renderWeather)}
             </tbody>
         </table>
-      
     );
   }
+
+  renderWeather(data) {
+      let name = data.city.name;
+      let tempList = data.list.map(weather => weather.main.temp);
+      let humidityList = data.list.map(weather => weather.main.humidity);
+      let windList = data.list.map(weather => weather.wind.speed);
+
+      tempList = tempList.map(temp => Math.round(temp - 273.15)); //converts kelvin to celcius
+
+      return (
+          <tr key={name}>
+              <td className="city-name">{name}</td>
+              <td><Graph data={tempList} units="&deg; C" color="orange"/></td>
+              <td><Graph data={humidityList} units="%" color="teal"/></td>
+              <td><Graph data={windList} units="Km/h" color="purple"/> </td>
+          </tr>
+      );
+    }
 }
 
-function mapStateToProps(state) {
-    return {
-        
-    }
+function mapStateToProps({weather}) {  //{weather} === state.weather
+    return {weather}; //{weather} === {weather: weather}
 }
 
 export default connect(mapStateToProps)(WeatherList);
