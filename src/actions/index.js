5e54eeae8b2c0c22002664f44eca396458c6426f
hitro11 <rohitnazareth07@gmail.com>
2022-05-14 21:28:24 -0400

    changed openweater api calls to https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '355c934f86b4621e42bfdb4dcdb91769';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
